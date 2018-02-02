cbacdb8f048f972a47dd0d1930cfb0bf47e22db9
Michael J. Stealey <michael.j.stealey@gmail.com>
2018-02-02 16:52:50 -0500

    sed allow /
    replace / with ! for sed statements that could have paths in them

--- 4.2.1/docker-entrypoint.sh
+++ 4.2.1/docker-entrypoint.sh
@@ -56,19 +56,19 @@ _vhost_conf () {
     local OUTFILE=/etc/httpd/davrods_conf.d/davrods-vhost.conf
     if [[ "${SSL_ENGINE,,}" == 'on' ]]; then
         # SSL settings
-        sed -i 's/#LoadModule ssl_module modules/mod_ssl.so/LoadModule ssl_module modules/mod_ssl.so/' $OUTFILE
-        sed -i 's/<VirtualHost *:80>/<VirtualHost *:443>/' $OUTFILE
-        sed -i 's/#SSLEngine off/SSLEngine on/' $OUTFILE
-        sed -i 's/#SSLCertificateFile ""/SSLCertificateFile '"${SSL_CERTIFICATE_FILE}"'/' $OUTFILE
-        sed -i 's/#SSLCertificateKeyFile ""/SSLCertificateKeyFile '"${SSL_CERTIFICATE_KEY_FILE}"'/' $OUTFILE
+        sed -i 's!#LoadModule ssl_module modules/mod_ssl.so!LoadModule ssl_module modules/mod_ssl.so!' $OUTFILE
+        sed -i 's!<VirtualHost \*:80>!<VirtualHost \*:443>!' $OUTFILE
+        sed -i 's!#SSLEngine off!SSLEngine on!' $OUTFILE
+        sed -i 's!#SSLCertificateFile ""!SSLCertificateFile '"${SSL_CERTIFICATE_FILE}"'!' $OUTFILE
+        sed -i 's!#SSLCertificateKeyFile ""!SSLCertificateKeyFile '"${SSL_CERTIFICATE_KEY_FILE}"'!' $OUTFILE
     fi
     # VirtualHost settings
-    sed -i 's/ServerName dav.example.com/ServerName '"${VHOST_SERVER_NAME}"'/' $OUTFILE
+    sed -i 's!ServerName dav.example.com!ServerName '"${VHOST_SERVER_NAME}"'!' $OUTFILE
     sed -i 's!<Location />!<Location '"${VHOST_LOCATION}"'>!' $OUTFILE
-    sed -i 's/#DavRodsServer localhost 1247/DavRodsServer '"${VHOST_DAV_RODS_SERVER}"'/' $OUTFILE
-    sed -i 's/#DavRodsZone tempZone/DavRodsZone '"${VHOST_DAV_RODS_ZONE}"'/' $OUTFILE
-    sed -i 's/#DavRodsAuthScheme Native/DavRodsAuthScheme '"${VHOST_DAV_RODS_AUTH_SCHEME}"'/' $OUTFILE
-    sed -i 's/#DavRodsExposedRoot  User/DavRodsExposedRoot  '"${VHOST_DAV_RODS_EXPOSED_ROOT}"'/' $OUTFILE
+    sed -i 's!#DavRodsServer localhost 1247!DavRodsServer '"${VHOST_DAV_RODS_SERVER}"'!' $OUTFILE
+    sed -i 's!#DavRodsZone tempZone!DavRodsZone '"${VHOST_DAV_RODS_ZONE}"'!' $OUTFILE
+    sed -i 's!#DavRodsAuthScheme Native!DavRodsAuthScheme '"${VHOST_DAV_RODS_AUTH_SCHEME}"'!' $OUTFILE
+    sed -i 's!#DavRodsExposedRoot  User!DavRodsExposedRoot  '"${VHOST_DAV_RODS_EXPOSED_ROOT}"'!' $OUTFILE
 }
 
 echo "IncludeOptional /etc/httpd/davrods_conf.d/*.conf" >> /etc/httpd/conf/httpd.conf
